d941e2ec493f0d017d2b546ea5aec59e0c34d0c3
ditokhrsmwn <dito.kharismawan@yahoo.com>
2024-04-02 02:35:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasakhir/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.example.tugasakhir;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(decorView.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, Login.class));
+                finish();
+            }
+        },3000);
+        }
+    }
